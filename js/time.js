403c94e13cc231d24262be181a7bc994cac31e69
FyodorShklyar <30theflame05@gmail.com>
2023-02-17 10:10:37 +0300

    feat: make showTime showDate func

---
+++ js/time.js
@@ -0,0 +1,23 @@
+const time = document.querySelector('.time')
+const dateHTML = document.querySelector('.date')
+const options = { month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', timeZone: 'UTC' }
+
+var date = new Date();
+var currentTime = date.toLocaleTimeString();
+var currentDate = date.toLocaleDateString('en-EN', options);
+
+function showTime() {
+    date = new Date();
+    currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    showDate();
+    setTimeout(showTime, 1000);
+}
+
+function showDate() {
+    date = new Date();
+    currentDate = date.toLocaleDateString('en-EN', options);
+    dateHTML.textContent = currentDate;
+}
+
+showTime();
